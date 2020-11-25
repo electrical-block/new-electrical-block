4a870259d145f31b7c4619f5b0a38726d02df1d4
darkfanxing <darkfanxing@gmail.com>
2020-11-26 05:05:24 +0800

    Delete HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <template>
-  <HelloWorld />
+  content
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
