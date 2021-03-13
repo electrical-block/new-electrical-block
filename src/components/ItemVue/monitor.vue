62bc4f94dd11f8a801dcbb68ec30c45517b69594
win0820 <wwiinn0820@gmail.com>
2021-03-13 13:24:30 +0800

    Modify monitor.vue

--- src/components/ItemVue/monitor.vue
+++ src/components/ItemVue/monitor.vue
@@ -1,18 +1,72 @@
 <template>
-  <rect
-    x="0"
-    y="0"
-    stroke="#2828FF"
-    stroke-width="2"
-    width="30"
-    height="30"
-  />
+  <g>
+    <rect
+      class="st0"
+      width="113.4"
+      height="113.4"
+    />
+    <path
+      class="st1"
+      d="M103,64.4H10.4c-2.7,0-4.9-2.2-4.9-4.9V12.7c0-2.7,2.2-4.9,4.9-4.9H103c2.7,0,4.9,2.2,4.9,4.9v46.8
+C107.9,62.2,105.7,64.4,103,64.4z"
+    />
+    <g>
+      <circle
+        class="st0"
+        cx="17.4"
+        cy="87"
+        r="8.6"
+      />
+      <circle
+        class="st2"
+        cx="17.4"
+        cy="87"
+        r="5.1"
+      />
+    </g>
+    <g>
+      <circle
+        class="st0"
+        cx="39.4"
+        cy="87.1"
+        r="8.6"
+      />
+      <circle
+        class="st3"
+        cx="39.4"
+        cy="87.1"
+        r="5.1"
+      />
+    </g>
+    <rect
+      x="57.2"
+      y="84.9"
+      class="st4"
+      width="48.2"
+      height="4.4"
+    />
+    <rect
+      x="96.2"
+      y="79.3"
+      class="st4"
+      width="4"
+      height="15.2"
+    />
+    <text
+      x="56"
+      y="38"
+      style="text-anchor: middle;  dominant-baseline: middle;"
+    >
+      {{ value }}
+    </text>
+  </g>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      value: 0
     }
   },
   methods: {
@@ -22,4 +76,9 @@ export default {
 </script>
 
 <style scoped>
+.st0{fill:#EFEFEF;stroke:#000000;stroke-miterlimit:10;}
+.st1{fill:#D3EDFB;stroke:#000000;stroke-miterlimit:10;}
+.st2{fill:#525066;stroke:#000000;stroke-miterlimit:10;}
+.st3{fill:#504E5B;stroke:#000000;stroke-miterlimit:10;}
+.st4{fill:#4D5C63;stroke:#000000;stroke-miterlimit:10;}
 </style>
